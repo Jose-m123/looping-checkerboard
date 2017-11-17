eafaa014a66848260cefb6497b624d6c4bfb8465
Jose <josem56020@students.laalliance.org>
2017-11-17 19:53:32 +0000

    Finished

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(500,400);
+}
+
+function draw() {
+  background(0);
+  fill(250);
+
+  var x = 50;
+  var count = 0
+
+  if(count%2 === 0){
+    fill('white')
+    }else{
+      fill('black')
+    }
+  }
+  for (var i = 0; i < 3; i++) {
+    rect(x, height/2, 40, 40);
+    x += 50;
+
+}
